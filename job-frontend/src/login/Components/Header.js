24cc753e3ff07f02127b6b623cc5d995beab8b3e
Chetan Sharma <73491258+chetan705@users.noreply.github.com>
2025-07-22 02:39:10 +0530

    Update Header.js

--- job-frontend/src/login/Components/Header.js
+++ job-frontend/src/login/Components/Header.js
@@ -14,7 +14,7 @@ function Header() {
                 className="bi bi-search"
                 style={{ fontSize: "1.75rem", margin: "0px 6px" }}
               ></i>
-              <h1>JOBHUNT </h1>
+              <h1>CareerLink </h1>
             </span>
           </Navbar.Brand>
           <Navbar.Toggle
